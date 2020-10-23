d5392a9220c789e9ac82ad03c63ae8d8ee67a38b
DenisLemeshko <lemeshko.denis@yandex.ru>
2020-10-23 17:53:02 +0300

    first lesson

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+	$('.top-slider').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: '<button type="button" class="slick-arow slick-next"><img src="images/next-arrow.svg" alt="next arrow"></button>',
+		prevArrow:'<button type="button" class="slick-arow slick-prev"><img src="images/prev-arrow.svg" alt="prev arrow"></button>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+
+				}
+			},
+
+			{
+				breakpoint: 640,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	});
+});
