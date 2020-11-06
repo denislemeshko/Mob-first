bd467018e5077a48603de20d8d45a531c53740b5
DenisLemeshko <lemeshko.denis@yandex.ru>
2020-11-06 12:32:24 +0300

    bottom-slider

--- js/main.js
+++ js/main.js
@@ -22,3 +22,28 @@ $(function(){
 		]
 	});
 });
+
+$(function(){
+	$('.bottom-slider').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: '<button type="button" class="slick-arow slick-next"><img src="images/next-arrow.svg" alt="next arrow"></button>',
+		prevArrow:'<button type="button" class="slick-arow slick-prev"><img src="images/prev-arrow.svg" alt="prev arrow"></button>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					arrows: false
+				}
+			},
+
+			{
+				breakpoint: 640,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	});
+});
